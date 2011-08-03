dba3a19a1f9e403728f5b10a4be570ea33e0f27e
jaredr <jared.roughan@zeacom.com>
2011-08-03 16:23:12 +1200

    Added TeamCity monitor (currently only returns project list)

---
+++ src/Emanate.Core/Input/TeamCity/TeamCity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+
+namespace Emanate.Core.Input.TeamCity
+{
+    public class TeamCityMonitor
+    {
+        private readonly TeamCityConnection connection = new TeamCityConnection("xxx", false);
+
+        public string GetProjects()
+        {
+            var uri = connection.CreateUri("/httpAuth/app/rest/projects");
+
+            return connection.Request(uri);
+        }
+    }
+
+    public class TeamCityConnection
+    {
+        private readonly Uri baseUri;
+
+        public TeamCityConnection(string hostName, bool isSsl)
+        {
+            var protocol = isSsl ? "https" : "http";
+            baseUri = new Uri(string.Format(CultureInfo.InvariantCulture, "{0}://{1}", protocol, hostName));
+        }
+
+        public Uri CreateUri(string relativeUrl)
+        {
+            if (relativeUrl.StartsWith("/"))
+                relativeUrl = relativeUrl.Substring(1);
+
+            return new Uri(baseUri, relativeUrl);
+        }
+
+        public string Request(Uri uri)
+        {
+            var webRequest = CreateWebRequest(uri);
+            webRequest.Accept = "application/xml";
+
+            using (WebResponse webResponse = webRequest.GetResponse())
+            using (var stream = webResponse.GetResponseStream())
+            using (var reader = new StreamReader(stream))
+                return reader.ReadToEnd();
+        }
+
+        public HttpWebRequest CreateWebRequest(Uri uri)
+        {
+            var webRequest = (HttpWebRequest)WebRequest.Create(uri);
+            // HACK: Remove hardcoding
+            webRequest.Credentials = new NetworkCredential("xxx", "xxx");
+            webRequest.Proxy = null;
+            return (webRequest);
+        }
+    }
+}
---
+++ src/Emanate.IntegrationTests/Input/teamcity.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using Emanate.Core.Input.TeamCity;
+using NUnit.Framework;
+
+namespace Emanate.IntegrationTests
+{
+    [TestFixture]
+    public class teamcity
+    {
+        [Test]
+        public void should_get_all_projects()
+        {
+            var monitor = new TeamCityMonitor();
+            var projects = monitor.GetAllProjects();
+
+            Assert.IsTrue(projects.Contains("Lync PBX"));
+        }
+    }
+}
