6fd6ef108784fb3cc8dfdb4608de8b9c38d0284f
jaredr <jared.roughan@zeacom.com>
2011-09-23 16:26:11 +1200

    Added some TC monitor unit tests

--- src/Emanate.UnitTests/Core/Input/TeamCity/teamcity_monitor.cs
+++ src/Emanate.UnitTests/Core/Input/TeamCity/teamcity_monitor.cs
@@ -10,21 +10,28 @@ namespace Emanate.UnitTests.Core.Input.TeamCity
     public class teamcity_monitor
     {
         [Test]
+        public void should_not_use_connection_until_monitoring_starts()
+        {
+            var configuration = new TeamCityConfiguration();
+            configuration.BuildsToMonitor = "ProjectName1";
+
+            Assert.DoesNotThrow(() => new TeamCityMonitor(null, configuration));
+        }
+
+        [Test]
         public void should_monitor_single_project()
         {
             var connection = new Mock<ITeamCityConnection>();
             string projectsXml =
 @"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""yes"" ?> 
 <projects>
-  <project name=""ProjectName1"" id=""project1"" href=""/httpAuth/app/rest/projects/id:project1"" /> 
-  <project name=""ProjectName2"" id=""project2"" href=""/httpAuth/app/rest/projects/id:project2"" /> 
-  <project name=""ProjectName3"" id=""project3"" href=""/httpAuth/app/rest/projects/id:project3"" /> 
+  <project name=""ProjectName1"" id=""project1"" /> 
 </projects>";
             string projectXml =
 @"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""yes"" ?> 
-<project webUrl=""http://teamcityuri/project.html?projectId=project2"" description="""" archived=""false"" name=""ProjectName1"" id=""project1"" href=""/httpAuth/app/rest/projects/id:project1"">
+<project name=""ProjectName1"" id=""project1"">
   <buildTypes>
-    <buildType id=""bt1"" name=""BuildName1"" href=""/httpAuth/app/rest/buildTypes/id:bt1"" projectName=""ProjectName1"" projectId=""project1"" webUrl=""http://teamcityuri/viewType.html?buildTypeId=bt1"" /> 
+    <buildType id=""bt1"" name=""BuildName1"" /> 
   </buildTypes>
 </project>";
             string runningBuildsXml =
@@ -32,7 +39,7 @@ namespace Emanate.UnitTests.Core.Input.TeamCity
             string buildXml =
 @"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""yes"" ?> 
 <builds>
-  <build id=""811"" number=""419"" status=""SUCCESS"" buildTypeId=""bt1"" startDate=""20110923T153954+1200"" href=""/httpAuth/app/rest/builds/id:811"" /> 
+  <build id=""811"" status=""SUCCESS"" buildTypeId=""bt1"" /> 
 </builds>";
             connection.Setup(c => c.GetProjects()).Returns(projectsXml);
             connection.Setup(c => c.GetProject("project1")).Returns(projectXml);
@@ -44,11 +51,49 @@ namespace Emanate.UnitTests.Core.Input.TeamCity
 
             monitor.BeginMonitoring();
 
-            Assert.AreEqual(BuildState.Succeeded, monitor.CurrentState);
+            Assert.AreNotEqual(BuildState.Unknown, monitor.CurrentState);
         }
 
         [Test]
-        public void should_fail_if_build_configuration_missing()
+        public void should_monitor_single_matching_project_if_multiple_projects_exist()
+        {
+            var connection = new Mock<ITeamCityConnection>();
+            string projectsXml =
+@"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""yes"" ?> 
+<projects>
+  <project name=""ProjectName1"" id=""project1"" /> 
+  <project name=""ProjectName2"" id=""project2"" /> 
+  <project name=""ProjectName3"" id=""project3"" /> 
+</projects>";
+            string projectXml =
+@"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""yes"" ?> 
+<project name=""ProjectName1"" id=""project1"">
+  <buildTypes>
+    <buildType id=""bt1"" name=""BuildName1"" /> 
+  </buildTypes>
+</project>";
+            string runningBuildsXml =
+@"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""yes"" ?><builds count=""0""></builds>";
+            string buildXml =
+@"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""yes"" ?> 
+<builds>
+  <build id=""811"" status=""SUCCESS"" buildTypeId=""bt1"" /> 
+</builds>";
+            connection.Setup(c => c.GetProjects()).Returns(projectsXml);
+            connection.Setup(c => c.GetProject("project1")).Returns(projectXml);
+            connection.Setup(c => c.GetRunningBuilds()).Returns(runningBuildsXml);
+            connection.Setup(c => c.GetBuild("bt1")).Returns(buildXml);
+            var configuration = new TeamCityConfiguration();
+            configuration.BuildsToMonitor = "ProjectName1:BuildName1";
+            var monitor = new TeamCityMonitor(connection.Object, configuration);
+
+            monitor.BeginMonitoring();
+
+            Assert.AreNotEqual(BuildState.Unknown, monitor.CurrentState);
+        }
+
+        [Test]
+        public void should_fail_if_build_missing_in_config()
         {
             var connection = new Mock<ITeamCityConnection>();
             string projectsXml =
@@ -62,6 +107,42 @@ namespace Emanate.UnitTests.Core.Input.TeamCity
             var monitor = new TeamCityMonitor(connection.Object, configuration);
 
             Assert.Throws<IndexOutOfRangeException>(monitor.BeginMonitoring);
+        }
+
+        [Test]
+        public void should_fail_if_build_status_unknown()
+        {
+            var connection = new Mock<ITeamCityConnection>();
+            string projectsXml =
+@"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""yes"" ?> 
+<projects>
+  <project name=""ProjectName1"" id=""project1"" /> 
+  <project name=""ProjectName2"" id=""project2"" /> 
+  <project name=""ProjectName3"" id=""project3"" /> 
+</projects>";
+            string projectXml =
+@"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""yes"" ?> 
+<project name=""ProjectName1"" id=""project1"">
+  <buildTypes>
+    <buildType id=""bt1"" name=""BuildName1"" /> 
+  </buildTypes>
+</project>";
+            string runningBuildsXml =
+@"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""yes"" ?><builds count=""0""></builds>";
+            string buildXml =
+@"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""yes"" ?> 
+<builds>
+  <build id=""811"" status=""XXX"" buildTypeId=""bt1"" /> 
+</builds>";
+            connection.Setup(c => c.GetProjects()).Returns(projectsXml);
+            connection.Setup(c => c.GetProject("project1")).Returns(projectXml);
+            connection.Setup(c => c.GetRunningBuilds()).Returns(runningBuildsXml);
+            connection.Setup(c => c.GetBuild("bt1")).Returns(buildXml);
+            var configuration = new TeamCityConfiguration();
+            configuration.BuildsToMonitor = "ProjectName1:BuildName1";
+            var monitor = new TeamCityMonitor(connection.Object, configuration);
+
+            Assert.Throws<Exception>(monitor.BeginMonitoring);
         }
     }
 }
