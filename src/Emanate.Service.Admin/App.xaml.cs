d1a82dc16cc4e1225c50ed637e9676d17bcf0bdc
JRoughan <jared@jroughan.com>
2013-06-19 19:24:35 +1200

    TC input now shows all the available projects and configs

--- src/Emanate.Service.Admin/App.xaml.cs
+++ src/Emanate.Service.Admin/App.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 using Autofac;
 using Emanate.Core;
 
--- src/Emanate.TeamCity/InputSelector/InputSelectorViewModel.cs
+++ src/Emanate.TeamCity/InputSelector/InputSelectorViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Xml.Linq;
 using Emanate.Core;
 
 namespace Emanate.TeamCity.InputSelector
@@ -14,13 +16,30 @@ namespace Emanate.TeamCity.InputSelector
 
         public override void Initialize()
         {
-            //connection.GetProjects();
-            var project = new ProjectViewModel();
-            project.Id = "MyProjectId";
-            project.Name = "MyProjectName";
-            project.Configurations.Add(new ProjectConfigurationViewModel { Id = "MyConfigId", Name = "MyConfigName" });
-            project.Configurations.Add(new ProjectConfigurationViewModel { Id = "MyConfigId2", Name = "MyConfigName2" });
-            Projects.Add(project);
+            var projectsXml = connection.GetProjects();
+            var foo = XElement.Parse(projectsXml);
+
+            foreach (var projectElement in foo.Elements())
+            {
+                var project = new ProjectViewModel();
+                project.Name = projectElement.Attribute("name").Value;
+
+                var buildXml = connection.GetProject(projectElement.Attribute("id").Value);
+                var buildRoot = XElement.Parse(buildXml);
+
+                var buildElements = from buildTypesElement in buildRoot.Elements("buildTypes")
+                                    from buildElement in buildTypesElement.Elements("buildType")
+                                    select buildElement;
+
+                foreach (var buildElement in buildElements)
+                {
+                    var coniguration = new ProjectConfigurationViewModel();
+                    coniguration.Name = buildElement.Attribute("name").Value;
+                    project.Configurations.Add(coniguration);
+                }
+
+                Projects.Add(project);
+            }
         }
 
         private ObservableCollection<ProjectViewModel> projects = new ObservableCollection<ProjectViewModel>();
