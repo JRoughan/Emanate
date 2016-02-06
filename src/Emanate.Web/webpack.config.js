530b9cc95284f767a16748469848595ac7782e6e
Jared Roughan <jared.roughan@readify.net>
2016-02-06 20:39:06 +1100

    Added babel plugins for new JS syntax/features

--- src/Emanate.Web/webpack.config.js
+++ src/Emanate.Web/webpack.config.js
@@ -12,6 +12,12 @@ const common = {
   // Entry accepts a path or an object of entries.
   // The build chapter contains an example of the latter.
   entry: PATHS.app,
+  // Add resolve.extensions.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without!!!
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
@@ -23,6 +29,15 @@ const common = {
         test: /\.css$/,
         loaders: ['style', 'css'],
         // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      },
+      // Set up jsx. This accepts js too thanks to RegExp
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        loaders: ['babel?cacheDirectory'],
         include: PATHS.app
       }
     ]
