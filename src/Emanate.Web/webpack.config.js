7910727d3981e4f38f1d178c4a218a209ccce0ee
Jared Roughan <jared.roughan@readify.net>
2016-02-06 19:34:29 +1100

    React/webpack skeleton for web UI

---
+++ src/Emanate.Web/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  // Entry accepts a path or an object of entries.
+  // The build chapter contains an example of the latter.
+  entry: PATHS.app,
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  }
+};
