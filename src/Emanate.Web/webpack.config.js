db462c742814b825248354e6bea177a1f7543a4d
Jared Roughan <jared.roughan@readify.net>
2016-02-06 20:00:06 +1100

    Configured the HMR plugin for real time updates

--- src/Emanate.Web/webpack.config.js
+++ src/Emanate.Web/webpack.config.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const path = require('path');
+const webpack = require('webpack');
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
@@ -19,7 +20,26 @@ const common = {
 
 // Default configuration
 if(TARGET === 'start' || !TARGET) {
-  module.exports = merge(common, {});
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+
 }
 
 if(TARGET === 'build') {
