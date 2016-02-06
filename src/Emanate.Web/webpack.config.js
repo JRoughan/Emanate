373f8553c111bb7d84fdb78b8a51b98cb2587983
Jared Roughan <jared.roughan@readify.net>
2016-02-06 19:42:10 +1100

    Added webpack-merge to easily split dev/release requirements

--- src/Emanate.Web/webpack.config.js
+++ src/Emanate.Web/webpack.config.js
@@ -1,11 +1,13 @@
+const merge = require('webpack-merge');
 const path = require('path');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   // Entry accepts a path or an object of entries.
   // The build chapter contains an example of the latter.
   entry: PATHS.app,
@@ -14,3 +16,12 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
+
+// Default configuration
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
