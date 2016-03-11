f1c976b96b1e38322e2cbe2126c2df2d2357e688
Jared Roughan <jared.roughan@readify.net>
2016-03-11 21:02:40 +1100

    Added modal dialog to configure inputs

--- src/Emanate.Web/app/components/Output.jsx
+++ src/Emanate.Web/app/components/Output.jsx
@@ -1,8 +1,14 @@
 ﻿import React from 'react';
+import Modal from 'react-modal';
 import OutputActions from '../actions/OutputActions';
 import Editable from './Editable.jsx';
 
 export default class Output extends React.Component {
+
+    state = {
+        modalIsOpen : false
+    };
+
     render() {
         const { output, onDelete } = this.props;
 
@@ -14,9 +20,23 @@ export default class Output extends React.Component {
                             onEdit={this.editOutput}
                             onDelete={onDelete.bind(null, output.id)} />
                 <div>{output.profile.name}</div>
+                <button onClick={this.openModal}>Inputs</button>
+                <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal} >
+                    <h2>{output.name} inputs<span><button onClick={this.closeModal}>x</button></span></h2>
+                    <button>Save</button>
+                    <button onClick={this.closeModal}>Cancel</button>
+                </Modal>
             </div>
         );
     }
+
+    openModal = () => {
+        this.setState({modalIsOpen: true});
+    };
+     
+    closeModal = () => {
+        this.setState({modalIsOpen: false});
+    };
 
     editOutput = (name) => {
         const outputId = this.props.output.id;
