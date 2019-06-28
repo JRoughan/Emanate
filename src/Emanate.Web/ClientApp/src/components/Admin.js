88d41a8f0c152df8e5f3044a4804d76ecc831ae7
Jared <jared@easyweddings.com.au>
2019-06-28 15:39:11 +1000

    Add card style for display devices

--- src/Emanate.Web/ClientApp/src/components/Admin.js
+++ src/Emanate.Web/ClientApp/src/components/Admin.js
@@ -3,6 +3,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { actionCreators } from '../store/Admin';
 import DisplayDeviceProfilePicker from './DisplayDeviceProfilePicker';
+import deviceIcon from '../images/deviceIcon.png'
 
 class Admin extends Component {
 
@@ -25,14 +26,21 @@ function renderDisplayDevices(props) {
     return (
         <div>
             <button className="btn btn-primary" onClick={() => props.addDisplayDevice({ name: 'New Device' })}>+</button>
-            <ul>
+            <div className="card-group">
                 {props.displayDevices.map(device =>
-                    <li key={device.id}>
-                        <h3>{device.name}<button className="btn btn-danger" onClick={() => props.removeDisplayDevice(device.id)}>X</button></h3>
-                        <DisplayDeviceProfilePicker />
-                    </li>
+                    <div className="card" key={device.id}>
+                        <img className="card-img-top" src={deviceIcon} alt="Device type" />
+                        <div className="card-body">
+                            <h3 className="card-title">{device.name}<button className="btn btn-danger" onClick={() => props.removeDisplayDevice(device.id)}>X</button></h3>
+                            Profile
+                            <DisplayDeviceProfilePicker />
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">Active</small>
+                        </div>
+                    </div>
                 )}
-            </ul>
+            </div>
         </div>
     );
 }
