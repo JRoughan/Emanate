61d1d92ddf6c8358e7171e072b40570ed2395bb1
ewJared <jared@easyweddings.com.au>
2019-07-25 23:01:22 +1000

    Convert Emanate.UnitTests to .NET Core

--- src/Emanate.UnitTests/Service/monitoring_service.cs
+++ src/Emanate.UnitTests/Service/monitoring_service.cs
@@ -1,59 +1,62 @@
-﻿//using Autofac.Features.Indexed;
-//using Emanate.Core;
-//using Emanate.Core.Configuration;
-//using Emanate.Core.Input;
-//using Emanate.Core.Output;
-//using Emanate.UnitTests.Builders;
-//using NSubstitute;
-//using Xunit;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Autofac.Features.Indexed;
+using Emanate.Core;
+using Emanate.Core.Configuration;
+using Emanate.Core.Input;
+using Emanate.Core.Output;
+using Emanate.UnitTests.Builders;
+using Emanate.Web;
+using NSubstitute;
+using Xunit;
 
-//namespace Emanate.UnitTests.Service
-//{
-//    public class monitoring_service
-//    {
-//        [Fact]
-//        public void should_not_begin_monitoring_when_started_if_no_mapping()
-//        {
-//            var buildMonitor = Substitute.For<IBuildMonitor>();
-//            var buildMonitorFactory = Substitute.For<IBuildMonitorFactory>();
-//            buildMonitorFactory.Create(Arg.Any<IInputDevice>()).Returns(buildMonitor);
-//            var buildMonitorFactories = Substitute.For<IIndex<string, IBuildMonitorFactory>>();
-//            buildMonitorFactories["key"].Returns(buildMonitorFactory);
+namespace Emanate.UnitTests.Service
+{
+    public class monitoring_service
+    {
+        [Fact]
+        public async Task should_not_begin_monitoring_when_started_if_no_mapping()
+        {
+            var buildMonitor = Substitute.For<IBuildMonitor>();
+            var buildMonitorFactory = Substitute.For<IBuildMonitorFactory>();
+            buildMonitorFactory.Create(Arg.Any<IInputDevice>()).Returns(buildMonitor);
+            var buildMonitorFactories = Substitute.For<IIndex<string, IBuildMonitorFactory>>();
+            buildMonitorFactories["key"].Returns(buildMonitorFactory);
 
-//            var service = new SourceMonitoringService(buildMonitorFactories);
+            var service = new SourceMonitoringService(new [] { buildMonitor });
 
-//            service.Start();
+            await service.StartAsync(default);
 
-//            buildMonitor.DidNotReceive().BeginMonitoring();
-//        }
+            await buildMonitor.DidNotReceive().BeginMonitoring();
+        }
 
-//        [Fact]
-//        public void should_begin_monitoring_when_started_if_mapping_exists()
-//        {
-//            var buildMonitor = Substitute.For<IBuildMonitor>();
-//            var buildMonitorFactory = Substitute.For<IBuildMonitorFactory>();
-//            buildMonitorFactory.Create(Arg.Any<IInputDevice>()).Returns(buildMonitor);
-//            var buildMonitorFactories = Substitute.For<IIndex<string, IBuildMonitorFactory>>();
-//            buildMonitorFactories["key"].ReturnsForAnyArgs(buildMonitorFactory);
+        [Fact]
+        public async Task should_begin_monitoring_when_started_if_mapping_exists()
+        {
+            var buildMonitor = Substitute.For<IBuildMonitor>();
+            var buildMonitorFactory = Substitute.For<IBuildMonitorFactory>();
+            buildMonitorFactory.Create(Arg.Any<IInputDevice>()).Returns(buildMonitor);
+            var buildMonitorFactories = Substitute.For<IIndex<string, IBuildMonitorFactory>>();
+            buildMonitorFactories["key"].ReturnsForAnyArgs(buildMonitorFactory);
 
-//            var outputDevice = new OutputDeviceBuilder().Build();
+            var outputDevice = new OutputDeviceBuilder().Build();
 
-//            var inputDevice = new InputDeviceBuilder().Build();
+            var inputDevice = new InputDeviceBuilder().Build();
 
-//            var mapping = new Mapping { OutputDeviceId = outputDevice.Id };
-//            mapping.InputGroups.Add(new InputGroup { InputDeviceId = inputDevice.Id, Inputs = { "Build1", "Build2" } });
+            var mapping = new Mapping { OutputDeviceId = outputDevice.Id };
+            mapping.InputGroups.Add(new InputGroup { InputDeviceId = inputDevice.Id, Inputs = { "Build1", "Build2" } });
 
-//            var config = new ConfigBuilder()
-//                .WithOutputDevice(outputDevice)
-//                .WithInputDevice(inputDevice)
-//                .WithMapping(mapping)
-//                .Build();
+            var config = new ConfigBuilder()
+                .WithOutputDevice(outputDevice)
+                .WithInputDevice(inputDevice)
+                .WithMapping(mapping)
+                .Build();
 
-//            var service = new SourceMonitoringService(config, buildMonitorFactories);
+            var service = new SourceMonitoringService(new[] { buildMonitor });
 
-//            service.Start();
+            await service.StartAsync(default);
 
-//            buildMonitor.Received().BeginMonitoring();
-//        }
-//    }
-//}
+            await buildMonitor.Received().BeginMonitoring();
+        }
+    }
+}
