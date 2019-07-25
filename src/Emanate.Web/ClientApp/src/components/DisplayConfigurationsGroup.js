64efb9196df2f93276837f1097f0370ea92cff1a
ewJared <jared@easyweddings.com.au>
2019-07-25 19:22:41 +1000

    Update Autofac to v4

--- src/Emanate.Web/ClientApp/src/components/DisplayConfigurationsGroup.js
+++ src/Emanate.Web/ClientApp/src/components/DisplayConfigurationsGroup.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import SourceConfigurationsGroup from './SourceConfigurationsGroup';
 
 class DisplayConfigurationsGroup extends Component {
 
