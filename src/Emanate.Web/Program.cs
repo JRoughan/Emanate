d5953490ada33c3d91d620e4a680bb6da0621731
ewJared <jared@easyweddings.com.au>
2019-07-25 20:11:51 +1000

    Set up Emanate to run as a service

--- src/Emanate.Web/Program.cs
+++ src/Emanate.Web/Program.cs
@@ -1,6 +1,10 @@
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using Emanate.Web.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
 
 namespace Emanate.Web
 {
@@ -8,11 +12,29 @@ namespace Emanate.Web
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().SeedData().Run();
-        }
+            var isService = !Debugger.IsAttached;
+            var pathToContentRoot = Directory.GetCurrentDirectory();
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            }
+
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(pathToContentRoot)
+                .UseStartup<Startup>()
+                .Build()
+                .SeedData();
+
+            if (isService)
+            {
+                host.RunAsService();
+            }
+            else
+            {
+                host.Run();
+            }
+        }
     }
 }
